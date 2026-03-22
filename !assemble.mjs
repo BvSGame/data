4663d1b857f967e5b3fc681560197b6bbf646dc1
liledix4 <ed@liledix.com>
2026-03-22 07:12:52 +0000

    0.1.504.7: Version management made easier with the special Node.js script.

---
+++ !assemble.mjs
@@ -0,0 +1,52 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { config } from './!config.mjs';
+
+const zeroPad = (num, places) => String( num ).padStart( places, '0' );
+
+const initial_VersionObject = JSON.parse(
+  readFileSync( config.filePath.version.json, { encoding: 'utf-8' } )
+);
+const dateObject = new Date();
+const dateSplit = dateObject.toISOString().split( /T|:/ );
+const today = dateSplit[ 0 ];
+const time = `${ zeroPad( dateObject.getUTCHours(), 2) }:${ zeroPad( dateObject.getUTCMinutes(), 2 ) }`;
+const hour = parseInt( dateSplit[ 1 ] );
+
+let output_VersionObject = {
+  version: null,
+  date: today,
+  time: time,
+  timestamp: dateObject.getTime(),
+  revision: config.version.revision,
+  milestone: config.version.milestone,
+  milestoneStage: config.version.milestoneStage,
+  milestoneStageVersion: null,
+  milestoneVersion: null,
+  hour: hour,
+};
+
+output_VersionObject.milestoneStageVersion = initial_VersionObject.milestoneStageVersion;
+if ( process.argv[ 2 ] === 'versionbump' )
+  output_VersionObject.milestoneStageVersion++;
+output_VersionObject.milestoneVersion = parseInt(
+  `${ output_VersionObject.milestoneStage }${ zeroPad( output_VersionObject.milestoneStageVersion, 2 ) }`
+);
+output_VersionObject.version = `${ output_VersionObject.revision }.${ output_VersionObject.milestone }.${ output_VersionObject.milestoneVersion }.${ output_VersionObject.hour }`;
+
+writeFileSync(
+  config.filePath.version.json,
+  JSON.stringify( output_VersionObject, undefined, 2 )
+);
+writeFileSync(
+  config.filePath.version.min,
+  output_VersionObject.version
+);
+writeFileSync(
+  config.filePath.wiki.template.newNote,
+  `---
+tags:
+  - EmptyPages
+version: ${ output_VersionObject.version }
+---
+`
+);
---
+++ !config.mjs
@@ -0,0 +1,21 @@
+export const config = {
+
+  version: {
+    revision: 0,
+    milestone: 1,
+    milestoneStage: 5,
+  },
+
+  filePath: {
+    version: {
+      min: './version_new',
+      json: './version.json',
+    },
+    wiki: {
+      template: {
+        newNote: './wiki/_Templates/New note.md',
+      },
+    },
+  },
+
+};
